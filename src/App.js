73a3894a312eade1e7a33bf661851c953cce37f7
SciDev5 <jhuber2048@gmail.com>
2021-05-23 15:40:10 -0400

    Add and learn to react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// @ts-check
+import logo from "./logo.svg";
+import "./App.scss";
+import { Route } from "react-router";
+import React from "react";
+import { Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/** @extends {React.Component<{},{_currentURL:string},{}>} */
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Route path="/yeet/" exact>
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <p> Edit <code>src/App.js</code> and save to reload. </p>
+                        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                            Learn React
+                        </a>
+                        <Link to="/">go back</Link>
+                    </header>
+                </Route>
+                <Route path="/" exact>
+                    root page lol
+                    <Link to="/yeet">go to other</Link>
+                </Route>
+                <Route path="/index.html" exact>
+                    root page lol
+                    <Link to="/yeet">go to other</Link>
+                </Route>
+            </div>
+        );
+    }
 }
 
 export default App;
